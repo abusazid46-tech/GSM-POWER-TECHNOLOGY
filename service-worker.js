a4da856c77da3dcc7f15b28b76e0d82b433bedd8
abusazid46-tech <abusazid46@gmail.com>
2026-02-20 01:02:47 +0530

    Add caching for static assets in service worker
    Implement caching strategy for service worker

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,23 @@
-self.addEventListener('install', e => {
-  self.skipWaiting();
+const CACHE_NAME = "gsm-power-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js",
+  "/icon-192.png",
+  "/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
